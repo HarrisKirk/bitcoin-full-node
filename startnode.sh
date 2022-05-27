3b459bbc977f4aefc20d06f26775f5f8ca43a1ef
Harris Kirk <cjtkirk@protonmail.com>
2022-05-26 20:08:25 -0400

    First pass at download script

---
+++ startnode.sh
@@ -0,0 +1,6 @@
+version=0.17.0
+wget https://bitcoincore.org/bin/bitcoin-core-${version}/SHA256SUMS.asc
+wget https://bitcoincore.org/bin/bitcoin-core-${version}/bitcoin-${version}-x86_64-linux-gnu.tar.gz
+
+
+echo "Finished"
