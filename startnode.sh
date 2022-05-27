26a29b076238044d204c3f25cf94357e42e4d0fb
Harris Kirk <cjtkirk@protonmail.com>
2022-05-27 14:26:03 -0400

    Use downloads folder

--- startnode.sh
+++ startnode.sh
@@ -1,6 +1,12 @@
+downloads=./downloads
+rm -rf $downloads
+mkdir $downloads
+
+
 version=23.0
-wget https://bitcoincore.org/bin/bitcoin-core-${version}/SHA256SUMS.asc
-wget https://bitcoincore.org/bin/bitcoin-core-${version}/bitcoin-${version}-x86_64-linux-gnu.tar.gz
+wget --output-document=${downloads}/SHA256SUMS.asc https://bitcoincore.org/bin/bitcoin-core-${version}/SHA256SUMS.asc
+
+wget --output-document=${downloads}/bitcoin-${version}-x86_64-linux-gnu.tar.gz https://bitcoincore.org/bin/bitcoin-core-${version}/bitcoin-${version}-x86_64-linux-gnu.tar.gz
 
 
 echo "Finished"
