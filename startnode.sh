02fbb1a6a3dc76dc3d447de7bf921ece168b4b30
Harris Kirk <cjtkirk@protonmail.com>
2022-05-27 16:47:50 -0400

    Add a bitcoin.conf file

--- startnode.sh
+++ startnode.sh
@@ -2,11 +2,14 @@ downloads=./downloads
 bitcoin_core=./bitcoin_core
 
 rm -rf $downloads $bitcoin_core
-mkdir $downloads $bitcoin_core
+mkdir $downloads $bitcoin_core ~/.bitcoin
 
 version=23.0
 wget --output-document=${downloads}/SHA256SUMS.asc https://bitcoincore.org/bin/bitcoin-core-${version}/SHA256SUMS.asc
 wget --output-document=${downloads}/bitcoin-${version}-x86_64-linux-gnu.tar.gz https://bitcoincore.org/bin/bitcoin-core-${version}/bitcoin-${version}-x86_64-linux-gnu.tar.gz
 
 tar -zxf ${downloads}/bitcoin-${version}-x86_64-linux-gnu.tar.gz --directory $bitcoin_core
+
+echo "chain=test" >> ~/.bitcoin/bitcoin.conf
+
 echo "Finished"
