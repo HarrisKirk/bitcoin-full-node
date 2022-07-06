dddb772dfc129547c9f33542fe6dca02671a8e9d
Harris Kirk <cjtkirk@protonmail.com>
2022-07-06 18:17:55 -0400

    Refactor move all subprocesses into bci_common

---
+++ bci-app/bci_common.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+"""
+Common functions related to spawning processes and parsing returned json
+"""
+import subprocess
+import json
+import logging
+
+def execute_cli(cmd):
+    logging.debug (' '.join(cmd))
+    completed_process = subprocess.run(cmd, cwd='.', check=True, shell=False, capture_output=True)
+    json_string = completed_process.stdout.decode()
+    if json_string == '':
+        return None
+    else:
+        json_object = json.loads(json_string)
+        logging.debug(json.dumps(json_object, indent=2))
+        return json_object
+
+def execute_ssh(linode_ip, remote_cmd):
+    cmd = ['ssh', '-o', 'StrictHostKeyChecking=no', f"root@{linode_ip}"] + remote_cmd
+    logging.debug (' '.join(cmd))
+    completed_process = subprocess.run(cmd, cwd='.', check=True, shell=False, capture_output=True)
+
+def execute_scp(remote_cmd):
+    cmd = ['scp', '-o', 'StrictHostKeyChecking=no'] + remote_cmd
+    logging.debug (' '.join(cmd))
+    completed_process = subprocess.run(cmd, cwd='.', check=True, shell=False, capture_output=True)
+
+def execute_sh(cmd):
+    p = subprocess.run(cmd, check=True, shell=True, capture_output=True)
+    return p.stdout.decode().rstrip()    
+
+
+
+
+
+
+
