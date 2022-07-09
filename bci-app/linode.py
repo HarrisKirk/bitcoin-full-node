17757ce7d8c6d04f40dfbf0b9e503ee7b7ef86fe
Harris Kirk <cjtkirk@protonmail.com>
2022-07-09 16:24:53 -0400

    feat: launch startnode.sh in test mode working ok

--- bci-app/linode.py
+++ bci-app/linode.py
@@ -100,15 +100,16 @@ def cleanup(linode_tags):
     remove_instances(linode_tags)
 
 
-def launch_bitcoind(linode_ip, vol_label):
+def launch_bitcoind(linode_ip, chain, vol_label):
+    logging.info(f"Launching bitcoind with chain={chain} and datadir=/mnt/{vol_label}")
     execute_ssh(linode_ip, "root", ["chmod", "-R", "770", f"/mnt/{vol_label}"])
     execute_ssh(linode_ip, "root", ["useradd", "-m", "--shell", "/bin/bash", "bitcoinuser"])
-    execute_ssh(linode_ip, "root", ["chown", "bitcoinuser", "-R", f"/mnt/{vol_label}"])
+    execute_ssh(linode_ip, "root", ["chown", "bitcoinuser:bitcoinuser", "-R", f"/mnt/{vol_label}"])
     execute_scp(["/opt/devops-bci/startnode.sh", f"root@{linode_ip}:/home/bitcoinuser"])
-    console_out = execute_ssh(
-        linode_ip, "root", ["-t", "sudo", "su", "bitcoinuser", "-c", "/home/bitcoinuser/startnode.sh"]
-    )
     execute_ssh(linode_ip, "root", ["chown", "bitcoinuser:bitcoinuser", "-R", f"/home/bitcoinuser/startnode.sh"])
+    console_out = execute_ssh(
+        linode_ip, "root", ["-t", "sudo", "su", "bitcoinuser", "-c", f"/home/bitcoinuser/startnode.sh"]
+    )
     logging.info(console_out)
     logging.info("Bitcoin node running.")
 
--- startnode.sh
+++ startnode.sh
@@ -1,24 +1,39 @@
+# if [ $# -ne 2 ]; then
+#   echo "Running $0 with arguments '$@'"
+#   echo 1>&2 "Usage:  $0 [chain] [datadir]"
+#   echo 1>&2 "Example $0 test /mnt/data"
+#   exit 3
+# fi
+# bitcoin_conf_chain=$1
+# bitcoin_conf_datadir=$2
+
+bitcoin_conf_chain=test
+bitcoin_conf_datadir=/mnt/btcvol-dev
+
 downloads=${HOME}/downloads
 bitcoin_core=${HOME}/bitcoin_core
-vol_label=btc_node_data
 
 rm -rf $downloads $bitcoin_core
-mkdir $downloads $bitcoin_core ${HOME}/.bitcoin
+rm ${HOME}/.bitcoin/bitcoin.conf
+rm -rf ${bitcoin_conf_datadir}
+mkdir $downloads $bitcoin_core ${HOME}/.bitcoin ${bitcoin_conf_datadir}
 
 version=23.0
 wget --output-document=${downloads}/SHA256SUMS.asc https://bitcoincore.org/bin/bitcoin-core-${version}/SHA256SUMS.asc
 wget --output-document=${downloads}/bitcoin-${version}-x86_64-linux-gnu.tar.gz https://bitcoincore.org/bin/bitcoin-core-${version}/bitcoin-${version}-x86_64-linux-gnu.tar.gz
 tar -zxf ${downloads}/bitcoin-${version}-x86_64-linux-gnu.tar.gz --directory $bitcoin_core
 echo "[INFO] Extract completed into ${bitcoin_core}"
-echo "chain=test" > ${HOME}/.bitcoin/bitcoin.conf
+
+echo "#Configuration created by '$0'"     >  ${HOME}/.bitcoin/bitcoin.conf
+echo "chain=${bitcoin_conf_chain}"      >> ${HOME}/.bitcoin/bitcoin.conf
+echo "datadir=${bitcoin_conf_datadir}"  >> ${HOME}/.bitcoin/bitcoin.conf
 echo "[INFO] ${HOME}/.bitcoin/bitcoin.conf configured"
+cat ${HOME}/.bitcoin/bitcoin.conf
+echo ""
 
 cd ${bitcoin_core}/bitcoin-${version}/bin
 echo "Launching bitcoind from $(pwd)..."
-./bitcoind -daemon
+./bitcoind  -daemon
 sleep 20
 ./bitcoin-cli getblockchaininfo # repeat until initialblockdownload is false
-# ./bitcoin-cli help
-ls -al
-
 echo "Finished"
