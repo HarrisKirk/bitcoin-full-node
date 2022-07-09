e4578c25c579a5229f6ce36c780545c0abf0baeb
Harris Kirk <cjtkirk@protonmail.com>
2022-07-09 18:07:30 -0400

    refactor: introduce bitcoin module

---
+++ bci-app/bitcoin.py
@@ -0,0 +1,17 @@
+from common import execute_ssh, execute_scp
+import logging
+
+def launch_bitcoind(linode_ip, chain, vol_label):
+    logging.info(f"Launching bitcoind with chain={chain} and datadir=/mnt/{vol_label}")
+    execute_ssh(linode_ip, "root", ["chmod", "-R", "770", f"/mnt/{vol_label}"])
+    execute_ssh(linode_ip, "root", ["useradd", "-m", "--shell", "/bin/bash", "bitcoinuser"])
+    execute_ssh(linode_ip, "root", ["chown", "bitcoinuser:bitcoinuser", "-R", f"/mnt/{vol_label}"])
+    execute_scp(["/opt/devops-bci/startnode.sh", f"root@{linode_ip}:/home/bitcoinuser"])
+    execute_ssh(linode_ip, "root", ["chown", "bitcoinuser:bitcoinuser", "-R", f"/home/bitcoinuser/startnode.sh"])
+    console_out = execute_ssh(
+        linode_ip, "root", ["-t", "sudo", "su", "bitcoinuser", "-c", f"/home/bitcoinuser/startnode.sh"]
+    )
+    logging.info(console_out)
+    logging.info("Bitcoin node running.")
+
+
--- bci-app/linode.py
+++ bci-app/linode.py
@@ -100,20 +100,6 @@ def cleanup(linode_tags):
     remove_instances(linode_tags)
 
 
-def launch_bitcoind(linode_ip, chain, vol_label):
-    logging.info(f"Launching bitcoind with chain={chain} and datadir=/mnt/{vol_label}")
-    execute_ssh(linode_ip, "root", ["chmod", "-R", "770", f"/mnt/{vol_label}"])
-    execute_ssh(linode_ip, "root", ["useradd", "-m", "--shell", "/bin/bash", "bitcoinuser"])
-    execute_ssh(linode_ip, "root", ["chown", "bitcoinuser:bitcoinuser", "-R", f"/mnt/{vol_label}"])
-    execute_scp(["/opt/devops-bci/startnode.sh", f"root@{linode_ip}:/home/bitcoinuser"])
-    execute_ssh(linode_ip, "root", ["chown", "bitcoinuser:bitcoinuser", "-R", f"/home/bitcoinuser/startnode.sh"])
-    console_out = execute_ssh(
-        linode_ip, "root", ["-t", "sudo", "su", "bitcoinuser", "-c", f"/home/bitcoinuser/startnode.sh"]
-    )
-    logging.info(console_out)
-    logging.info("Bitcoin node running.")
-
-
 def create_instance(linode_tags):
     linode_root_pass = os.getenv("LINODE_ROOT_PASSWORD")
     ssh_key = execute_sh("cat ~/.ssh/id_rsa.pub")
